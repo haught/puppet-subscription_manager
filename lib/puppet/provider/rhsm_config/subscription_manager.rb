dc982d0df76c69b8a8db29dd4605c8dabe0148fa
Matt Haught <dmhaught@ncsu.edu>
2019-01-03 16:58:30 -0500

    force integers

--- lib/puppet/provider/rhsm_config/subscription_manager.rb
+++ lib/puppet/provider/rhsm_config/subscription_manager.rb
@@ -143,7 +143,7 @@ Puppet::Type.type(:rhsm_config).provide(:subscription_manager) do
   # @api private
   def self.parse_digit(section, title, digit)
     value = if Puppet::Type.type(:rhsm_config).binary_options.key? "#{section}_#{title}".to_sym
-              (digit == '1') ? true : false
+              (digit == '1') ? 1 : 0
             else
               digit.to_i
             end
